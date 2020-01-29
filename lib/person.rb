1374c03fc55aaf55c0dbbe15c9a3ae2c7df57cf6
123JackCole <123jack.cole@gmail.com>
2020-01-29 14:39:40 -0800

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,73 @@
-# your code goes here
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account
+    attr_writer :happiness, :hygiene
+
+    def happiness
+        @happiness.clamp(0,10)
+    end
+
+    def hygiene 
+        @hygiene.clamp(0,10)
+    end 
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happy?
+        if self.happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def clean?
+        if self.hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end 
+
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(person)
+        self.happiness += 3
+        person.happiness += 3
+        "Hi #{person.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == 'politics'
+            self.happiness -= 2 
+            person.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == 'weather'
+            self.happiness += 1
+            person.happiness += 1
+            "blah blah sun blah rain"
+        else 
+            'blah blah blah blah blah'
+        end
+    end 
+end
